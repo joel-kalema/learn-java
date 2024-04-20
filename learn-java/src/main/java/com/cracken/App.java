9a29eae4d29e3469d55ce10fc5b708da736d69fb
joel kalema <joelkalema63@gmail.com>
2024-04-20 19:00:00 +0200

    updated the numbers

--- learn-java/src/main/java/com/cracken/App.java
+++ learn-java/src/main/java/com/cracken/App.java
@@ -8,7 +8,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello joel!" );
-        System.out.print(27 + " \n joel's age \n");
+        String phrase;
+        int age = 27;
+        double nbre1 = 10, nbre2 = 2;
+
+        int result = (int) (nbre1 / nbre2);
+
+        String stg = new String();
+        String string = new String(" when I'll celebrate");
+        stg = " my favorite age";
+        phrase = "hello joel";
+        System.out.println( phrase.charAt(3) + " " + result);
+        System.out.print(age + stg + string);
     }
 }
